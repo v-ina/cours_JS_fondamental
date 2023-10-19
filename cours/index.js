18da96dad7939a3f4f19430e13fc54514acfe4a1
yeonsookang <yeonsoo.kang@lapiscine.pro>
2023-10-19 18:41:18 +0200

    fin de cours 19 oct

--- cours/index.js
+++ cours/index.js
@@ -186,12 +186,14 @@ if(is30YearsOld){           // ligne pour vrai
 
 
 
-
+/*
 const bigNb1 = 100
 const bigNb2 = 400
+*/
 
 // Exo_ Q. ecrire la condition pour afficher le plus grand nombre
 
+/*
 if(bigNb2 > bigNb1){
     console.log(`${bigNb2} est plus grand`)
 } else{
@@ -205,11 +207,11 @@ if(compare) {
 } else {
     console.log(`${bigNb1} est plus grand`)
 }
-
+*/
 
 
 // Exo_ A. 
-
+/*
 const resultComparison = bigNb1 > bigNb2
 
 if(resultComparison){
@@ -225,10 +227,12 @@ if(bigNb1 > bigNb2){
 } else if ( bigNb1 == bigNb2) {
     console.log(bigNb1,bigNb2)
 }
+*/
 // 조금 더 이해하기 위한 예시. 보통 수학적 비교이므로 세번째 비교가 따로 필요없음.
 
 
 
+/*
 const firstname = `paul`
 // const firstname2 = "hamid"
 // const firstname3 = "pauline"
@@ -238,5 +242,162 @@ if (firstname == `hamid`){
 } else if(firstname == `pauline`){
     console.log(`bonjour + ${firstname}`)
 } else {
-    console.log(`bonjour ${firstname}`)
+    // console.log('bonjour' + firstname + ', je ne connaisais pas ton prenom avant.')
+    // Exo_ Q. reecrire le message logge avec des backticks
+    console.log(`bonjour ${firstname}, je ne connaissais pas ton prenom avant.`)
 }
+*/
+
+
+
+// VARIABLE COMPLEXE : LES TABLEAUX (Array)
+
+
+/*
+let arr1 = [3, 5, 12, 204.5, 'hello', false, 'world']
+const firstElement = arr1[0]
+console.log(firstElement)
+console.log(arr1[4])
+// console.log(arr1[0,4])는 안되고 (arr1[0], arr[4]) 이렇게 두개 써야함. 앞에꺼처럼 쓰면 4 값만 가져옴.
+
+let arrResult1 = arr1[0] + arr1[1]
+console.log(arrResult1) 
+// 요렇게 하면 테이블 안에 있는 요소들끼리 불러와서 붙일 수 있겠구만.
+
+let arrResult2 = arr1[4] + ' ' + arr1[6]
+let arrResult3 = `${arr1[4]} - ${arr1[6]}`
+let arrResult4 = `arr1[4] - arr1[6]`   
+// 이래서 변수의 값을 불러올 때는 $를 써야 하는구나. '값'을 의미해서 달러로 쓰는건가보다 ㅋㅋ
+
+console.log(arrResult2) 
+console.log(arrResult3)
+console.log(arrResult4) 
+그리고 일단 이렇게 여러가지 종류의 변수들을 다 같은 
+표에 적는게 흔하진 않음
+*/
+
+
+// 내 답
+// if(${arr1[5]} == false){
+//     arrResult5 = `${arr1[4]} ${arr1[6]}`
+// } else {
+//     arrResult5 = $arr1[1] + $arr1[3]
+// }
+// console.log(arrResult5)
+
+
+
+// 정답
+/*
+let arrResult5
+// 이 줄이 let이고 우리가 나중에 이 값을 변경할 수 있어서 이 줄에서 값을 주지 않아도 괜찮은데 const arrResult5 적어두고 값을 바로 주지 않으면 아예 오류가 남. const 속성 자체가 절대로 값이 바뀌지 않는 애니까
+
+if (arr1[5]) {      // 우리 arr1에 5번째 요소가 false이므로 이 줄을 false인 경우의 상태
+    arrResult5 = `${arr1[4]} ${arr1[6]}`
+} else {            // 반대로 여기는 true인 상탱
+    arrResult5 = arr1[1] + arr1[3]
+}
+console.log(arrResult5)
+
+
+if(0), if(2), if(4) 이런식으로 작성이 되면 
+0이 아닌 모든 수를 true로 인식하기 때문에 
+false, true, true의 값을 갖는다.
+만약에 chaine을 사용한다고 하면, 
+if('0'), if('fiejf') 같이 값이 들어간 애들은 true로 
+생각하는데 if('')는 false 값을 갖는다. 
+왜냐면 얘는 undefined이기 때문. undefined 와 null은
+false의 값을 갖는다.
+*/
+
+// 표에 뭔지 확인하고 바꾸는게 아니고 바로 ecraser시키기
+/*
+let arr1 = [
+    3, 
+    5, 
+    12, 
+    204.5, 
+    'hello',
+    false, 
+    'world'
+]
+let str = arr1[4]
+arr1[4] = "bonjour"
+console.log(arr1)
+
+//Exo_ Q. changer la valeur de 204.5 en 190 par soustraction de 14.5
+
+arr1[3] = 190
+console.log(arr1)
+
+
+arr1[3] -= 14.5
+// arr1[3] = arr1[3] -14.5 이랑 완전 같은거임
+// 이렇게 안에 있는 요소에 값에서 빼기하고 싶으면 -=로 하기 
+console.log(arr1)
+*/
+
+// VARIABLE COMPLEXE : LES OBJETS cle(프로퍼티라고 부른다)와  :와 값 이렇게 3개가 요소임. 각 프로퍼티는 콤마로 구분한다. 불러오는 방법은 점을 찍어서. 얘는 순서는 딱히 중요하지가 않다. 키가 있으므로.
+let obj1 = {
+    maPremierePropriete : 12,
+    maDeuxiemePropriete : 14,
+    prop3 : 11,
+    prop9 : 'hello'
+}
+
+console.log(obj1.maPremierePropriete)
+console.log(obj1.prop9)
+
+
+let identity = {
+    firstname: 'paul',
+    lastname: 'Doazan',
+    age: 35,
+    town: 'talence'
+}
+
+// A l'aide de l'objet precedent, construire la chaine de caractere 
+// ==> Je m'appelle paul Doazan, j'ai 35 ans et j'habite a talence.
+
+
+let resultat = `Je m'appelle ${identity.firstname} ${identity.lastname}, j'ai ${identity.age} ans et j'habite a ${identity.town}.`
+
+console.log(resultat)
+
+
+// creer un deuxieme objet identity2, stocket identity et identity2 dans un tableau, et ecrire la phrase precedente en utilisant le tableau.
+
+/*
+let identity2 ={
+    firstname: 'Pierre',
+    lastname: 'Durand',
+    age: 30,
+    town: 'bordeaux'
+}
+
+let arrIdentities = [
+    identity.firstname,
+    identity.lastname,
+    identity.age,
+    identity.town
+]
+
+let arrIdentitiesResult = ` Je m'appelle ${arrIdentities[0]} ${arrIdentities[1]}, j'ai ${arrIdentities[2]} ans et j'habite a ${arrIdentities[3]}.`
+
+console.log(arrIdentitiesResult)
+*/
+
+// A.
+
+let identity2 ={
+    firstname: 'Pierre',
+    lastname: 'Durand',
+    age: 30,
+    town: 'bordeaux'
+}
+
+let arrIdentities = [identity, identity2]
+
+let arrIdentitiesResult = `Je m'appelle ${arrIdentities[1].firstname} ${arrIdentities[1].lastname}, j'ai ${arrIdentities[1].age} ans et j'habite a ${arrIdentities[1].town}.`
+
+console.log(arrIdentitiesResult)
